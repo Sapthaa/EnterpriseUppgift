903b72c7256f852a9d3a3863cae9e8101bbfecc7
André sapthawisukhon <andre.saptha@hotmail.com>
2025-02-08 17:27:12 +0100

    Update CustomUserDetailsService error handling

--- src/main/java/com/sapthaa/webserviceuppgift/service/CustomUserDetailsService.java
+++ src/main/java/com/sapthaa/webserviceuppgift/service/CustomUserDetailsService.java
@@ -11,22 +11,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final LoggerService logger;
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
+    public CustomUserDetailsService(LoggerService logger, UserRepository userRepository) {
+        this.logger = logger;
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        CustomUser user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        try {
+            CustomUser user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Användare hittades inte: " + username));
 
-        return User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .roles(user.getRole())
-                .build();
+            logger.info("Användare hittad: {}" + user.getUsername());
+
+            return User.builder()
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRole())
+                    .build();
+        } catch (UsernameNotFoundException e) {
+
+            logger.error("Fel uppstod vid inläsning av användare med användarnamn: {}" + username, e);
+            throw e;
+        } catch (Exception e) {
+
+            logger.error("Oförutsett fel uppstod vid inläsning av användare med användarnamn: {}" + username, e);
+            throw new RuntimeException("Oförutsett fel uppstod", e);
+        }
     }
-
 }
